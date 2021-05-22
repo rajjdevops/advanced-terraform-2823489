f3ca84c21de66eb9518c1c2f6c096facd330354c
rajjdevops <65726128+rajjdevops@users.noreply.github.com>
2021-05-22 15:36:21 +1000

    Update variables.tf

--- 03_01_remotestate/variables.tf
+++ 03_01_remotestate/variables.tf
@@ -1,9 +1,6 @@
 # //////////////////////////////
 # VARIABLES
 # //////////////////////////////
-variable "aws_access_key" {}
-
-variable "aws_secret_key" {}
 
 variable "region" {
   default = "us-east-2"
